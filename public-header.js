f0a24196a03e201427dda29954ca9c095370f8af
Wookie119 <andy.gifford@movemotorcycles.co.uk>
2026-09-20 15:28:10 +0100

    Refresh multilingual page language controller

--- public-header.js
+++ public-header.js
@@ -34,7 +34,7 @@ function loadAnyBikePublicPageLanguageController(){
     }
 
     const script=document.createElement("script");
-    script.src="/public-page-language.js?v=20260920-6";
+    script.src="/public-page-language.js?v=20260920-7";
     script.async=false;
     script.dataset.anybikePageLanguage="true";
     script.onload=function(){
